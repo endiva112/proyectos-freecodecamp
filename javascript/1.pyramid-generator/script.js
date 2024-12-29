86814d5a682ed5479ed2d41bf64935e1b630e5a3
endiva112 <diazvalenzuelaenrique@gmail.com>
2024-12-29 21:03:57 +0100

    Primer uso carpeta js

---
+++ javascript/1.pyramid-generator/script.js
@@ -0,0 +1,12 @@
+/*Como declarar varaible:
+# Los nombres de las variables siguen reglas específicas: los nombres pueden incluir letras, números, signos de dólar y guiones bajos,
+pero no pueden contener espacios y no deben comenzar con un número.
+
+# Cuando los nombres de las variables son más de una palabra, existen convenciones de nomenclatura específicas para la forma de escribir 
+las palabras en mayúsculas. En JavaScript, la convención que se debe utilizar es la de mayúsculas y minúsculas.
+
+*/
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
---
+++ javascript/README.md
@@ -0,0 +1,11 @@
+# FreeCodeCamp Projects
+
+Esta carpeta la componen los programas que desarrollé y con la que me certifiqué en "Algoritmos y Estructuras de Datos de JavaScript", 
+donde aprendí los conceptos básicos de JavaScript, como variables, matrices, objetos, bucles, funciones, DOM y más.
+
+También se hace uso de la Programación Orientada a Objetos (POO), Programación Funcional, pensamiento algorítmico, 
+cómo trabajar con almacenamiento local y cómo obtener datos mediante una API.
+
+## Proyectos
+
+- [PENDING](#): Under development.
