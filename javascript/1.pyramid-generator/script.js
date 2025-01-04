da2952c07bdca88811185c4943f88580cfaefac3
endiva112 <diazvalenzuelaenrique@gmail.com>
2025-01-04 17:11:03 +0100

    js update

--- javascript/1.pyramid-generator/script.js
+++ javascript/1.pyramid-generator/script.js
@@ -1,11 +1,3 @@
-/*Como declarar varaible:
-# Los nombres de las variables siguen reglas específicas: los nombres pueden incluir letras, números, signos de dólar y guiones bajos,
-pero no pueden contener espacios y no deben comenzar con un número.
-
-# Cuando los nombres de las variables son más de una palabra, existen convenciones de nomenclatura específicas para la forma de escribir 
-las palabras en mayúsculas. En JavaScript, la convención que se debe utilizar es camelcase "ejemploUno".
-
-*/
 
 const character = "#";
 const count = 8;
@@ -14,3 +6,38 @@ const rows = [];
 for (let i = 0; i < count; i = i + 1) {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Como declarar varaible:
+# Los nombres de las variables siguen reglas específicas: los nombres pueden incluir letras, números, signos de dólar y guiones bajos,
+pero no pueden contener espacios y no deben comenzar con un número.
+
+# Cuando los nombres de las variables son más de una palabra, existen convenciones de nomenclatura específicas para la forma de escribir 
+las palabras en mayúsculas. En JavaScript, la convención que se debe utilizar es camelcase "ejemploUno".
+
+# La palabra clave "function" le indica a JavaScript que la variable "myFunction" será una función. "parameter" es una variable que representa 
+un valor que se pasa a la función cuando se utiliza. Una función puede tener tantos o tan pocos parámetros como desee. Al igual que un bucle 
+for, el espacio entre las llaves es el cuerpo de la función.
+ex:
+
+    function myFunction(parameter) {
+        //code goes here
+    }
+
+
+*/
