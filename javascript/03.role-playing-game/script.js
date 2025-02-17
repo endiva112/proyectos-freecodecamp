629f3919d316e2ccbf6af91cc172b93b7336e920
endiva112 <diazvalenzuelaenrique@gmail.com>
2025-02-17 02:23:29 +0100

    project update

--- javascript/03.role-playing-game/script.js
+++ javascript/03.role-playing-game/script.js
@@ -82,7 +82,12 @@ const locations = [
     "button functions": [restart, restart, restart], 
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
   },
-  
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+  }
 ];
 
 // initialize buttons
@@ -260,5 +265,9 @@ function pickEight() {
 }
 
 function pick(guess) {
-  
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:";
 }
